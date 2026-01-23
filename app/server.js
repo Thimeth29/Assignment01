93cb8c0bd55c9e5550f35a49b8cb1330e2133f8b
Your Name <your.email@example.com>
2026-01-23 16:53:31 +0530

    name change

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node server – heloooooooo');
+  res.send('Hello from Node server – sithumi');
 });
 
 app.listen(port, '0.0.0.0', () => {
