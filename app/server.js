271fde18e2fd9e0f1213ce4d24f48c04d33c2671
Thimeth29 <thimethofficial2@gmail.com>
2026-01-23 16:19:58 +0530

    server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node server – Thimeth');
+  res.send('Hello from Node server – Chathnuka');
 });
 
 app.listen(port, '0.0.0.0', () => {
